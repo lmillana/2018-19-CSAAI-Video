5d8c9bc7dffb1b338f74245c86b4ed22a253f22c
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-03-28 10:58:57 +0100

    Empezando!

---
+++ controls.js
@@ -0,0 +1,42 @@
+
+
+function main(){
+  console.log("Probando...")
+
+  //-- VIDEO 1:
+  video1 = document.getElementById('video1');
+
+  //-- Tamaño del video:
+  video1.width = 500;
+  video1.height = 250;
+
+  //-- VIDEO 2:
+  video2 = document.getElementById('video2');
+
+  //-- Tamaño del video:
+  video2.width = 500;
+  video2.height = 250;
+
+  //-- VIDEO 3:
+  video3 = document.getElementById('video3');
+
+  //-- Tamaño del video:
+  video3.width = 500;
+  video3.height = 250;
+
+
+  /*
+  audio = document.getElementById('audio');
+
+  //-- Cuando el raton pase por encima del audio, que suene:
+  audio.onmouseover = () => {
+    console.log('Audio ON!')
+    audio.style.backgroundColor = "lightblue"
+  }
+
+  audio.onmouseout = () => {
+    console.log('Audio OFF')
+    audio.style.backgroundColor = ""
+  } */
+
+}
